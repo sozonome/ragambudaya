1700bca1e3d5288808f56b9a8884fc473f15fa20
sozonome <berserk.nate@gmail.com>
2019-12-17 03:41:19 +0700

    High Score Done

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Quiz } from './tebak-gambar/quiz.model';
 import { TebakGambarService } from './tebak-gambar/tebak-gambar.service';
 import { WayangService } from './infografik-wayang/wayang.service';
+import { Router } from '@angular/router';
+import { LoadingController, AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -13,11 +15,16 @@ export class HomePage {
 
   constructor(
     private tebakGambarServices: TebakGambarService,
-    private wayangSvc: WayangService
+    private wayangSvc: WayangService,
   ) {}
 
   ionViewWillEnter(){
     this.tebakGambarServices.fetchQuiz().subscribe();
+    this.tebakGambarServices.fetchScores().subscribe();
     this.wayangSvc.fetchWayang().subscribe();
+  }
+
+  tebakGambar(){
+    this.tebakGambarServices.tebakGambar();
   }
 }
--- src/app/home/tebak-gambar/after-quiz/after-quiz.page.ts
+++ src/app/home/tebak-gambar/after-quiz/after-quiz.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Score } from '../quiz.model';
+import { TebakGambarService } from '../tebak-gambar.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-after-quiz',
@@ -6,10 +9,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./after-quiz.page.scss'],
 })
 export class AfterQuizPage implements OnInit {
+  scores: Score[];
 
-  constructor() { }
+  constructor(
+    private tebakGambarServices: TebakGambarService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
   }
 
+  ionViewWillEnter(){
+    this.scores = this.tebakGambarServices.getAllScores();
+  }
+
+  checkFetch(){
+    console.log(this.scores);
+  }
+
+  playAgain(){
+    this.router.navigate(['', 'tebak-gambar']);
+  }
+
+  home(){
+    this.router.navigate(['']);
+  }
 }
